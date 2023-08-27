5e5649cf3ee65a11b2ea5eeb93da6095a0c3a291
Srivatsan <srivatsan1234567@gmail.com>
2023-08-27 18:27:46 +0530

    moved firebase initialised to become abstract

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:learningdart/firebase_options.dart';
 import 'package:learningdart/services/auth/auth_user.dart';
 import 'package:learningdart/services/auth/auth_providers.dart';
 import 'package:learningdart/services/auth/auth_exceptions.dart';
@@ -95,5 +97,11 @@ class FirebaseAuthProvider implements AuthProvider {
     } else {
       throw UserNotLoggedInAuthException();
     }
+  }
+
+  @override
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
   }
 }
